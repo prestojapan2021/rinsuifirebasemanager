c57a98b257c5f778b6f15471b61e523b106b1dab
prestojapan2021 <36428714+prestojapan2021@users.noreply.github.com>
2023-01-28 16:53:47 +0900

    Firebase初期化済

---
+++ android/app/src/main/kotlin/com/example/rinsuifirebasemanager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rinsuifirebasemanager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initFire();
+  }
+
+  Future<void> initFire() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void _incrementCounter() async {
+    QuerySnapshot<Map<String, dynamic>> docs = await FirebaseFirestore.instance.collection('購入権利').get();
+    docs.docs.forEach((element) {
+      // List<String> keys = element.data().keys.toList();
+      String key2 = element.data()['deviceId'];
+      String key3 = element.data()['name'];
+      String key4 = element.data()['subscription'];
+      String key5 = element.data()['token'];
+      String key6 = element.data()['userId'];
+      int key1 = element.data()['client_remaining_money'] as int;
+
+    });
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
